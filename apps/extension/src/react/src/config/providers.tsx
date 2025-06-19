35d624847e68cf4aeb2b6ed35b96f6e6d6d127d1
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2025-06-19 17:26:45 -0500

    add staleTime of five minutes to react query client

--- apps/extension/src/react/src/config/providers.tsx
+++ apps/extension/src/react/src/config/providers.tsx
@@ -1,11 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { queryClient } from './react-query';
+import { queryClient } from '@/config/react-query';
 import { AppSettingsProvider } from '@/hooks/use-app-settings';
 import { StopwatchProvider } from '@/hooks/use-stopwatch';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 export function Providers({ children }: React.PropsWithChildren) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
+    <QueryClientProvider client={queryClient}>
       <AppSettingsProvider>
         <StopwatchProvider>{children}</StopwatchProvider>
       </AppSettingsProvider>
